7f196dd06f0a5fb23e9f4a945db9ac8d3fcf41aa
Mr-Morgan <zmorgan163@gmail.com>
2020-05-06 09:19:38 +0300

    C-API widgets version task #6, #7

--- PythonScriptSupportQtWidgets/mainwindow.cpp
+++ PythonScriptSupportQtWidgets/mainwindow.cpp
@@ -10,9 +10,9 @@ ls = buffer.split(" ")
 max_len = 0
 word = ""
 for el in ls:
-   world_len = len(el)
+   word_len = len(el)
    if word_len > max_len:
-      max_len = world_len
+      max_len = word_len
       word = el
 buffer = word + " (" + str(max_len) + ")"
 
@@ -41,34 +41,27 @@ MainWindow::~MainWindow()
     delete ui;
 }//~MainWindow();
 
-void MainWindow::on_pushButton_clicked()
+QString MainWindow::dateTime(MainWindow::dateFlag flag) const
 {
-    //Буфер
-    QString buffer = "buffer = \"%1\"";
-    buffer = buffer.arg(ui->textEdit->toPlainText());
-
-    //Журнал
-    QString log = "buffer:\n" + buffer + "\n\n";
-
-    buffer.replace(QString("\n"), QString("\\n"));
-
     //Строка содержащая скрипт
-    QString script = ui->pyScriptEdit->toPlainText();
-    QStringList list = script.split('\n');
+    QString script = "import datetime\n"
+                     "buffer = datetime.datetime.today().strftime(\"%1\")";
 
-    log += "script:\n" + script + "\n\n";
+    QString arg = "???";
 
-    script = "";
-    for (auto i : list)
-        if(i[0] != '#') script += i + '\n';
-    script = script.left(script.length()-1);
+    switch (flag) {
+    case dAll: arg = "%m.%d.%Y %H:%M:%S";
+        break;
+    case dOnlyDate: arg = "%m.%d.%Y";
+        break;
+    case dOnlyTime: arg = "%H:%M:%S";
+        break;
+    }//switch (flag)
+
+    script = script.arg(arg);
 
     //Строка содержащая код на языке Python
-    QString python_code = "import Buffer\n" + buffer + "\n" + script + "\n" + "Buffer.setBuffer(buffer)";
-
-    log += "buffer:\n" + buffer + "\n\n";
-    log += "script:\n" + script + "\n\n";
-    log += "python_code:\n" + python_code + "\n\n";
+    QString python_code = "import Buffer\n" + script + "\n" + "Buffer.setBuffer(buffer)";
 
     //Исполнение строки в Python интерпретаторе
     PyRun_SimpleString(python_code.toStdString().c_str());
@@ -82,7 +75,188 @@ void MainWindow::on_pushButton_clicked()
         file.close();
     }//if (file.open(QIODevice::ReadOnly | QIODevice::Text))
 
-    ui->resultEdit->setPlainText(buff);
-    ui->logEdit->setTextColor(QColor("#8f5902"));
-    ui->logEdit->setText(log);
+    return buff;
+}//QString dateTime(dateFlag flag);
+
+QString MainWindow::registerChange(QString &buffer)
+{
+    if(buffer.length()) {
+
+        //Строка содержащая скрипт
+        QString script = "???";
+
+        switch (type) {
+        case rUpper: script = "buffer = buffer.upper()";
+            break;
+        case rTitle: script = "buffer = buffer.title()";
+            break;
+        case rLower: script = "buffer = buffer.lower()";
+            break;
+        }//switch (type)
+
+        //Строка содержащая код на языке Python
+        QString python_code = "import Buffer\nbuffer = \"%1\"\n" + script + "\n" + "Buffer.setBuffer(buffer)";
+
+        //Исполнение строки в Python интерпретаторе
+        PyRun_SimpleString(python_code.arg(buffer).toStdString().c_str());
+
+        //Вывод данных измененных Python скриптом из файла
+        QString buff = "???";
+        QFile file(bufFileName);
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QTextStream stream(&file);
+            buff = stream.readAll();
+            file.close();
+        }//if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+
+        type++;
+        return buff;
+    } else return buffer;
+}//QString registerChange(QString &buffer) const;
+
+QString MainWindow::fourSpacesInsteadTabs(QString &buffer)
+{
+    if(buffer.length()) {
+        //Строка содержащая скрипт
+        QString script = "buffer = buffer.replace(\'\\t\', \'    \')";
+
+        //Строка содержащая код на языке Python
+        QString python_code = "import Buffer\nbuffer = \"%1\"\n" + script + "\n" + "Buffer.setBuffer(buffer)";
+
+        //Исполнение строки в Python интерпретаторе
+        PyRun_SimpleString(python_code.arg(buffer).toStdString().c_str());
+
+        //Вывод данных измененных Python скриптом из файла
+        QString buff = "???";
+        QFile file(bufFileName);
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QTextStream stream(&file);
+            buff = stream.readAll();
+            file.close();
+        }//if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+
+        return buff;
+    } else return buffer;
+}//QString fourSpacesInsteadTabs(QString &buffer);
+
+void MainWindow::on_pushButton_clicked()
+{
+    //Буфер
+    QString buffer = "buffer = \"%1\"";
+    QString arg = ui->textEdit->toPlainText();
+
+    if(arg.length()) {
+
+        if(arg[arg.length()-1] == '\n') arg = arg.left(arg.length()-1);
+
+        //Журнал
+        QString log = "buffer:\n" + buffer + "\n\n";
+
+        arg.replace(QString("\\"), QString("\\\\"));
+        arg.replace(QString("\""), QString("\\\""));
+        arg.replace(QString("\n"), QString("\\n"));
+        arg.replace(QString("\t"), QString("\\t"));
+
+        buffer = buffer.arg(arg);
+
+        //Строка содержащая скрипт
+        QString script = ui->pyScriptEdit->toPlainText();
+        QStringList list = script.split('\n');
+
+        log += "script:\n" + script + "\n\n";
+
+        script = "";
+        for (auto i : list)
+            if(i[0] != '#') script += i + '\n';
+        script = script.left(script.length()-1);
+
+        //Строка содержащая код на языке Python
+        QString python_code = "import Buffer\n" + buffer + "\n" + script + "\n" + "Buffer.setBuffer(buffer)";
+
+        log += "buffer:\n" + buffer + "\n\n";
+        log += "script:\n" + script + "\n\n";
+        log += "python_code:\n" + python_code + "\n\n";
+
+        //Исполнение строки в Python интерпретаторе
+        PyRun_SimpleString(python_code.toStdString().c_str());
+
+        //Вывод данных измененных Python скриптом из файла
+        QString buff = "???";
+        QFile file(bufFileName);
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QTextStream stream(&file);
+            buff = stream.readAll();
+            file.close();
+        }//if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+
+        ui->resultEdit->setPlainText(buff);
+        ui->logEdit->setTextColor(QColor("#8f5902"));
+        ui->logEdit->setText(log);
+    }//if(arg.length())
 }//void on_pushButton_clicked();
+
+void MainWindow::on_dateTimeAllButton_clicked()
+{
+    ui->textEdit->insertPlainText(dateTime(dateFlag::dAll));
+}//void on_dateTimeAllButton_clicked();
+
+void MainWindow::on_dateTimeOnlyDateButton_clicked()
+{
+    ui->textEdit->insertPlainText(dateTime(dateFlag::dOnlyDate));
+}//void on_dateTimeOnlyDateButton_clicked();
+
+void MainWindow::on_dateTimeOnlyTimeButton_clicked()
+{
+    ui->textEdit->insertPlainText(dateTime(dateFlag::dOnlyTime));
+}//void on_dateTimeOnlyTimeButton_clicked();
+
+void MainWindow::on_UpperLowerTitleButton_clicked()
+{
+    //Получаем буффер из выделенного текста
+    QTextCursor cursor = ui->textEdit->textCursor();
+    QString buffer = cursor.selectedText();
+    int32_t start = cursor.selectionStart(),
+            end = cursor.selectionEnd();
+
+    buffer.replace(QString("\\"), QString("\\\\"));
+    buffer.replace(QString("\""), QString("\\\""));
+    buffer.replace(QString("\n"), QString("\\n"));
+    buffer.replace(QString("\t"), QString("\\t"));
+
+    QString text = ui->textEdit->toPlainText();
+
+    text = text.left(start) + registerChange(buffer) + text.right(text.length() - end);
+
+    ui->textEdit->setPlainText(text);
+
+    //Возвращаем выделение
+    cursor.setPosition(start);
+    cursor.movePosition(QTextCursor::NextCharacter, QTextCursor::KeepAnchor, end - start);
+    ui->textEdit->setTextCursor(cursor);
+
+}//void on_UpperLowerTitleButton_clicked();
+
+void MainWindow::on_fourSpacesButton_clicked()
+{
+    //Получаем буффер из выделенного текста
+    QTextCursor cursor = ui->textEdit->textCursor();
+    QString buffer = cursor.selectedText();
+    int32_t start = cursor.selectionStart(),
+            end = cursor.selectionEnd();
+
+    buffer.replace(QString("\\"), QString("\\\\"));
+    buffer.replace(QString("\""), QString("\\\""));
+    buffer.replace(QString("\n"), QString("\\n"));
+    buffer.replace(QString("\t"), QString("\\t"));
+
+    QString text = ui->textEdit->toPlainText();
+
+    text = text.left(start) + fourSpacesInsteadTabs(buffer) + text.right(text.length() - end);
+
+    ui->textEdit->setPlainText(text);
+
+    //Возвращаем выделение
+    cursor.setPosition(start);
+    cursor.movePosition(QTextCursor::NextCharacter, QTextCursor::KeepAnchor, end - start);
+    ui->textEdit->setTextCursor(cursor);
+}//void on_fourSpacesButton_clicked();
--- PythonScriptSupportQtWidgets/mainwindow.h
+++ PythonScriptSupportQtWidgets/mainwindow.h
@@ -12,6 +12,15 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+enum registerType {rUpper, rTitle, rLower};
+
+inline registerType& operator++(registerType &eRT, int)
+{
+   const int i = static_cast<int>(eRT) + 1;
+   eRT = static_cast<registerType>((i) % 3);
+   return eRT;
+}//inline registerType& operator++(registerType &eDOW, int)
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -19,13 +28,26 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    enum dateFlag {dAll, dOnlyDate, dOnlyTime};
+    //Функция возвращающая текущую дату в зависимости от флага
+    QString dateTime(dateFlag flag) const;
+    //Функция возвращающая строку с измененным регистром upper/title/lower
+    QString registerChange(QString &buffer);
+    //Функция возвращающая строку с заменой табуляции на 4 пробела
+    QString fourSpacesInsteadTabs(QString &buffer);
 
 private slots:
     void on_pushButton_clicked();
+    void on_dateTimeAllButton_clicked();
+    void on_dateTimeOnlyDateButton_clicked();
+    void on_dateTimeOnlyTimeButton_clicked();
+    void on_UpperLowerTitleButton_clicked();
+    void on_fourSpacesButton_clicked();
 
 private:
     Ui::MainWindow *ui;
     const char *bufFileName = "buffer.buf";
+    registerType type = rUpper;
 };//class MainWindow : public QMainWindow
 
 #endif // MAINWINDOW_H
